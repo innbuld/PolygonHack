98b6ca8b58458cd1072b6a67c514d152e8174bc6
innbuld <kanmiolanrewaju@gmail.com>
2022-11-27 13:38:31 -0800

    updating

--- pages/Account.js
+++ pages/Account.js
@@ -1,33 +1,31 @@
 import React from 'react'
 import { Button, Flex, Heading, Text, Box } from "@chakra-ui/react";
 import{AiFillPlusSquare} from "react-icons/ai"
+import{RiRefund2Fill} from "react-icons/ri"
 
 export default function Account() {
   return (
     <Flex w={'full'} justifyContent={"center"}  p='36' height={'100'} position='relative'  >
-      <Box borderWidth={'2px'} borderRadius={'lg'} shadow='2xl' backgroundColor='ash' p={'12'} mt='14' mr='58' rounded={'20'} >
+      <Box borderWidth={'2px'} borderRadius={'lg'} shadow='2xl' backgroundColor='ash' p={'12'} mt='14' mr='24' rounded={'20'} >
         <Box mt='-12' fontSize={'2xl'}   >
-         <Text >Upload </Text>
+         <Text >Uploads </Text>
          <Flex>
-         <Box cursor={'pointer'} >
+         <Box cursor={'pointer'} ml='2' mt='8' bottom={'3'} >
            <AiFillPlusSquare></AiFillPlusSquare>
          </Box>
          </Flex>
          
-        
-       
-        
 
         </Box>        
      </Box>
 
-     <Flex>
-     <Box borderWidth={'2px'} borderRadius={'lg'} shadow='2xl' backgroundColor='ash' p={'12'} mt='14' mr='96%' rounded={'20'} >
-        <Box mt='-12' fontSize={'2xl'}   >
-         <Text >Upload </Text>
+     <Flex w={'full'} >
+     <Box borderWidth={'2px'} borderRadius={'lg'} shadow='2xl' backgroundColor='ash' p={'12'} mt='14' mr={'28%'} rounded={'20'} >
+        <Box mt='-12' fontSize={'2xl'}  >
+         <Text >Funds </Text>
          <Flex>
-         <Box cursor={'pointer'} >
-           <AiFillPlusSquare></AiFillPlusSquare>
+         <Box cursor={'pointer'} mr='20' mt={'6'} >
+           <RiRefund2Fill></RiRefund2Fill>
          </Box>
          </Flex>
          
