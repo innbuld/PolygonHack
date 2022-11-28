08ac181de393c6d14362cd837aa7dd03cfd3da58
innbuld <kanmiolanrewaju@gmail.com>
2022-11-28 04:47:16 -0800

    update

--- pages/Account.js
+++ pages/Account.js
@@ -5,9 +5,9 @@ import{RiRefund2Fill} from "react-icons/ri"
 
 export default function Account() {
   return (
-    <Flex  width={'256'} justifyContent={'space-between'} alignItems="center" gap={'2'} p='32' >
+    <Flex  width={'256'} justifyContent={'space-between'} alignItems="center" p='32' >
       
-      <Box borderWidth={'2px'} borderRadius={'lg'} height='32' shadow='2xl' backgroundColor='ash' p={'12'} mb='56' mr='24' rounded={'20'}>
+      <Box borderWidth={'2px'} borderRadius={'lg'} height='32' shadow='dark-lg' backgroundColor='ash' p={'12'} mb='72' mr='24' rounded={'20'}>
         <Box mt='-12' fontSize={'3xl'} >
           <Text as={'b'} >Uploads </Text>
           <Box cursor={'pointer'} ml='8' mt='8'  >
@@ -16,19 +16,22 @@ export default function Account() {
         </Box>
       </Box>
 
-      <Box borderWidth={'2px'} borderRadius={'lg'} height='32' shadow='2xl' backgroundColor='ash' p={'12'} mb='56' mr='24' rounded={'20'}>
+      <Box borderWidth={'2px'} borderRadius={'lg'} height='32' shadow='dark-lg' backgroundColor='ash' p={'12'} mb='72' mr='24' rounded={'20'}>
         <Box mt='-12' fontSize={'3xl'} >
           <Text as={'b'} >Funds </Text>
-          <Box cursor={'pointer'} ml='8' mt='8'  >
+          <Box cursor={'pointer'}  mt='8'  >
              <RiRefund2Fill></RiRefund2Fill>
           </Box>
         </Box>
       </Box>
 
-     <Flex>
-       <Box mt={'56'} borderWidth={'2px'} ml='-96'  borderRadius={'lg'} height='32' shadow='2xl' backgroundColor='ash' p={'12'}   rounded={'20'} >
-        <Box  >
-          <Text>ncdm</Text>
+     <Flex ml={'-52'}  >
+       <Box mt={'56'} borderWidth={'2px'} ml='-96'   borderRadius={'lg'} height='60' shadow={'dark-lg'} backgroundColor='ash' p={'12'}  width='sm' rounded={'20'} >
+        <Box mt={'-12'} borderBottomWidth='2px'>
+          <Heading>Latest Activity</Heading>
+          <Flex>
+            <Text>ff</Text>
+          </Flex>
         </Box>
        </Box>
      </Flex>
