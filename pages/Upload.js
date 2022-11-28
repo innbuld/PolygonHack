cd600faef15781764a0495570b4006f5bcd2b571
innbuld <kanmiolanrewaju@gmail.com>
2022-11-28 05:04:10 -0800

    upload

--- pages/Upload.js
+++ pages/Upload.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import { Button, Flex, Heading, Text, Box } from "@chakra-ui/react";
+import{AiFillPlusSquare} from "react-icons/ai"
+
 
 export default function Upload() {
   return (
-    <Box>
-        dd
-    </Box>
+   <Flex justifyContent={'center'} width='full' height={'100'}>
+     <Box  height='32' shadow='dark-lg' mt='52'  p={'12'}  mr='24' rounded={'20'}>
+        <Box mt='-8' fontSize={'3xl'} >
+          <Text as={'b'} >Upload </Text>
+          <Box cursor={'pointer'} ml='8' mt='2'  >
+           <AiFillPlusSquare></AiFillPlusSquare>
+          </Box>
+        </Box>
+      </Box>
+
+
+
+
+
+
+
+
+   </Flex>
   )
 }
